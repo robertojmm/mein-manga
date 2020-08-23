c19b5611edaa1831baaa8312592ba77d4a0379aa
robertojmm <robertojmunozdeleon@gmail.com>
2020-08-23 13:56:53 +0200

    chore(cleaning): Removed console.log

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   async validate(payload: any) {
-    console.log(payload.sub);
+    console.log(payload);
     return { userId: payload.sub, username: payload.username };
   }
 }

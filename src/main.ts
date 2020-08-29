b869811786428875c03b61028a65b86ac1adde77
robertojmm <robertojmunozdeleon@gmail.com>
2020-08-29 12:30:27 +0200

    feat(main): changed app to listen to env indicated port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(env.NEST_PORT);
 }
 bootstrap();
